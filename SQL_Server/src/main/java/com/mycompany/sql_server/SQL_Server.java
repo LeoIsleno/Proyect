6414458dde3bd28a1d6619f0368f155b792a2625
Leo Isleno <Leoisleno01@gmail.com>
2023-06-07 23:23:57 -0300

    mensaje

---
+++ SQL_Server/src/main/java/com/mycompany/sql_server/SQL_Server.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sql_server;
+
+/**
+ *
+ * @author leo
+ */
+public class SQL_Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
